f4f25abf712618a92a8c23cd4ce29e6308ad05e9
Debasish Bose <devbose2008@gmail.com>
2017-09-20 22:20:29 -0500

    chapter 1

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -6,26 +6,44 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+  TextView price;
+  int quantity = 0;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    price = findViewById(R.id.price);
+  }
 
-    }
+  /**
+   * This method is called when the order button is clicked.
+   */
+  public void submitOrder(View view) {
+    //display(quantity);
+    displayPrice(quantity * 5);
+  }
 
-    /**
-     * This method is called when the order button is clicked.
-     */
-    public void submitOrder(View view) {
-        display(1);
-    }
+  private void displayPrice(int quantity) {
 
-    /**
-     * This method displays the given quantity value on the screen.
-     */
-    private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
-    }
+    price.setText(""+quantity);
+  }
+
+  /**
+   * This method displays the given quantity value on the screen.
+   */
+  private void display(int number) {
+    TextView quantityTextView = findViewById(R.id.quantity_text_view);
+    quantityTextView.setText("" + number);
+  }
+
+  public void increment(View view) {
+
+    quantity += 1;
+    display(quantity);
+  }
+
+  public void decrement(View view) {
+    quantity -= 1;
+    display(quantity);
+  }
 }
